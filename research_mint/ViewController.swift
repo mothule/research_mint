27c3c9a7d9a75636e18cd4bd889385727e440632
Motoki Kawakami <mothule.dev+github@gmail.com>
2020-07-31 16:54:48 +0900

    Initial xc proj

---
+++ research_mint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  research_mint
+//
+//  Created by motoki kawakami on 2020/07/31.
+//  Copyright © 2020 mothule. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
